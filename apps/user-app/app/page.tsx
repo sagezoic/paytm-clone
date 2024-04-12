d6dd9bc886d8602ed28a7dd39b91564ea9b90733
Rakesh Pandey <rakesh.pandey@pattern.com>
2024-04-12 06:04:38 +0530

    minor changes

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,9 +2,6 @@
 
 import { useBalance } from "@repo/stores/balance";
 
-import { PrismaClient } from "../../../packages/db";
-const prisma = new PrismaClient();
-
 export default function Page(): JSX.Element {
   return <div className="text-xl">Hello from Tailwind</div>;
 }
